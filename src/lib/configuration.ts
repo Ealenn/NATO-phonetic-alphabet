01ea64a97c821a5be05e25d67c9d72ee4f3d4a20
Marchandise Rudy <rudy.marchandise@protonmail.com>
2023-08-19 12:00:12 +0200

    fix: project configuration and github link (#2)

---
+++ src/lib/configuration.ts
@@ -0,0 +1,9 @@
+export const Configuration = {
+  Project: {
+    Name: 'NATO Phonetic Alphabet'
+  },
+  Github: {
+    Title: 'Ealenn/NATO-phonetic-alphabet',
+    Link: 'https://github.com/Ealenn/NATO-phonetic-alphabet'
+  }
+};
